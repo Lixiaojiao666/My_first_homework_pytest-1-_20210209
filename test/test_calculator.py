cd3fa40fd2e9678e82ff35d221d620136adf2646
Lxiaojiao666 <727991861@qq.com>
2021-02-10 21:42:24 +0800

    第一次上传作业 python pytest 测试实战（1）

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -22,21 +22,15 @@ def teardown_module():
 
 
 # 从calculator.yml文件中获取需要那部分测试数据['add']['datas']
-def get_datas():
+def get_datas(name, type='int'):
     with open("./datas/calculator.yml", 'rb') as f:
-        datas = yaml.safe_load(f)
-    return (datas['add']['datas'], datas['add']['ids'])
-
-
-def get_datas1():
-    with open("./datas/calculator.yml", 'rb') as f:
-        datas1 = yaml.safe_load(f)
-    return (datas1['div']['datas'], datas1['div']['ids'])
-
+        all_datas = yaml.safe_load(f)
+    datas = all_datas[name][type]['datas']
+    ids = all_datas[name][type]['ids']
+    return (datas, ids)
 
 def setup_function():
     print("(每次函数执行前都执行)资源准备：setup function")
-
 
 def teardown_function():
     print("(每次函数执行后都执行)资源销毁：teardown function")
@@ -44,12 +38,18 @@ def teardown_function():
 
 class TestCalculator:
     # :list 类型提示，可以提示该类型的一些参数或用法
-    datas: list = get_datas()
-    datas1: list = get_datas1()
+    # datas: list = get_datas()
+    add_int_data = get_datas('add', 'int')
+    add_float_data = get_datas('add', 'float')
+    div_int_normal_data = get_datas('div', 'int_normal')
+    div_int_error_data = get_datas('div', 'int_error')
+    div_float_data = get_datas('div', 'float')
 
     # 前置条件
     def setup_class(self):
         print("开始计算")
+        # 创建一个实例，调用pythoncode目录下的Calculator.py 文件下的 Calculator类
+        # 实例化一个 calc
         self.calc = Calculator()
 
     # 后置条件
@@ -58,24 +58,36 @@ class TestCalculator:
 
     # 测试方法：相加
     # 参数化装饰器
-    @pytest.mark.parametrize("a,b,result", datas[0], ids=datas[1])
-    def test_add(self, a, b, result):
-        # 创建一个实例，调用pythoncode目录下的Calculator.py 文件下的 Calculator类
-        # 实例化一个 calc
+    @pytest.mark.parametrize("a,b,result", add_int_data[0], ids=add_int_data[1])
+    def test_add_int(self, a, b, result):
         print(f"a={a},b={b},result={result}")
-        assert result == self.calc.add(a, b)
+        assert result == round(self.calc.add(a, b), 2)
+
+    @pytest.mark.parametrize("a,b,result", add_float_data[0], ids=add_float_data[1])
+    def test_add_float(self, a, b, result):
+        print(f"a={a},b={b},result={result}")
+        assert result == round(self.calc.add(a, b), 2)
 
     # 测试方法：相除
-    @pytest.mark.parametrize("a,b,result", datas1[0], ids=datas1[1])
-    def test_div(self, a, b, result):
-        if b == 0:
-            try:
-                self.calc.div(a, b)
-            except ZeroDivisionError as e:
-                print("除数不能为0")
-        else:
+    @pytest.mark.parametrize("a,b,result", div_int_normal_data[0], ids=div_int_normal_data[1])
+    def test_div_int_normal(self, a, b, result):
+        print(f"a={a},b={b},result={result}")
+        assert result == self.calc.div(a, b)
+
+    @pytest.mark.parametrize("a,b,result", div_int_error_data[0], ids=div_int_error_data[1])
+    def test_div_int_error(self, a, b, result):
+        with pytest.raises(ZeroDivisionError):
             print(f"a={a},b={b},result={result}")
             assert result == self.calc.div(a, b)
+
+    #    if b == 0:
+    #        try:
+    #            self.calc.div(a, b)
+    #        except ZeroDivisionError as e:
+    #            print("除数不能为0")
+    #    else:
+    #        print(f"a={a},b={b},result={result}")
+    #        assert result == self.calc.div(a, b)
 
     def setup(self):
         print("开始计算")
